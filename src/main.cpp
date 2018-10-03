1a78db118c3a35b031d4bda6cbc0caad304c2639
raavus-funkmaster <dau12@pitt.edu>
2018-10-03 19:40:09 -0400

    put init and close in their own functions, added event loop and main loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,25 @@
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
 
-int main(int argc, char* argv[]) {
+// initialize window and surfaces
+SDL_Window* window = NULL;
+SDL_Surface* screen_surface = NULL;
+SDL_Surface* media_surface = NULL;
 
-	// Make our window and surface
-	SDL_Window* window = NULL;
-	SDL_Surface* surf = NULL;
+// start up sdl, make a window
+/*bool init();
 
-	// Initialize SDL
+// load media
+bool load_media();
+
+// free media memory and close sdl
+void close();*/
+
+bool init()
+{
+	bool success = false;
+
+	// initialize sdl
 	if(SDL_Init (SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
 		printf("SDL failed to initialize. Error: %s\n", SDL_GetError());
 
@@ -19,27 +31,53 @@ int main(int argc, char* argv[]) {
 	{
 		// Assign our window 
 		window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
 		if (window == NULL)
 			printf("Window creation failed. Error: %s\n", SDL_GetError());
-
 		else
 		{
-			// begin interacting with window surface
-			surf = SDL_GetWindowSurface(window);
+			screen_surface = SDL_GetWindowSurface(window);
+			success = true;
+		}
+	}
+	return success;
+}
+
+void close()
+{
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+
+	bool quit = false;
+	SDL_Event e;
+
+	// initializes SDL and assigns the window. if this fails, it'll error message and exit
+	if (init())
+	{
+		// our main loop which updates the screen every 5 seconds. we aren't drawing anything new,
+		// so nothing changes
+		while (!quit) 
+		{
+			 // does the user want to quit?
+			 while(SDL_PollEvent(&e) != 0)  
+			 	if(e.type == SDL_QUIT)  
+			 		quit = true;
 
 			// fills the screen with a green rectangle
-			SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0x55, 0xFF, 0x99));
+			SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0x55, 0xFF, 0x99));
 
 			// updates the surface with our newly drawn information
 			SDL_UpdateWindowSurface(window);
 
-			// wait 5 seconds
-			SDL_Delay(5000);
+			// wait 5 milliseconds
+			SDL_Delay(5);
 		}
 
 		// free memory and quit
-		SDL_DestroyWindow(window);
-		SDL_Quit();
+		close();	
 	}
 
 	return 0;
